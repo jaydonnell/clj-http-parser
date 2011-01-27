017324168c869b66a9fdb624182f119e31e228ff
ddonnell <ddonnell@attinteractive.com>
2011-01-27 12:29:17 -0800

    first commit

---
+++ src/clj_html_parser/core.clj
@@ -0,0 +1,1 @@
+(ns clj-html-parser.core)
---
+++ test/clj_html_parser/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-html-parser.test.core
+  (:use [clj-html-parser.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
