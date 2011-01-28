52c5471838a543bc3235fbb7a4eec93c2f288687
ddonnell <ddonnell@attinteractive.com>
2011-01-28 09:46:59 -0800

    bumped version and updated readme

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-html-parser "0.1.0-SNAPSHOT"
+(defproject clj-html-parser "0.1.0"
   :description "html parser for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
